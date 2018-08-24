d152a5d97f7c27ead289a796098e317e28e5db0b
Augusto Furlan <gulyfurlan@gmail.com>
2018-08-23 23:20:27 -0300

    New functions

--- main.c
+++ main.c
@@ -16,12 +16,12 @@
  * Adicionar user adduser username so root pode - OK
  * Bloquear usuario block username so root pode - OK
  * Desbloquear username unlock username so root pode - OK
- * Remover user deluser username so root pode - apagar dir ? 
- * Exibir a data date - pode ser junto com hr ?
- * Exibir a hora time - - pode ser junto com hr ?
- 
+ * Remover user deluser username so root pode - OK
+ * Exibir a data date - OK
+ * Exibir a hora time - OK
  * Remover Diretorio (Verificar se existe filho)
- * o usuario so pode editar as pastas abaixo dele
+ * o usuario so pode editar as pastas abaixo dele - OK
+ 
  * Encerrar programa (poweroff) apenas o root pode fazer
  * Salvar todos os dados em arquivo para nao perder na hora reiniciar o programa
  */
@@ -35,6 +35,7 @@ typedef struct {
     char name[9];
     int father_id;
     int property_id;
+    char datetime[50];
     struct Dirs *prox;
     
 } Dirs;
@@ -59,7 +60,7 @@ typedef struct {
 void Inicializar_Dirs (Dirs *inicio);
 void Inicializar_Users (Users *users);
 void Inserir (Dirs *inicio);
-void Listar (Dirs *inicio);
+void Listar (Dirs *inicio, Users *ini_users);
 void Remove (Dirs *inicio);
 void PWD (Dirs *inicio);
 void CD (Dirs *inicio);
@@ -75,7 +76,10 @@ void BlockUsername (Users *inicio);
 void UnlockUsername (Users *inicio);
 void RmR (Dirs *inicio);
 void DelUser (Users *inicio);
-
+int VerifyProperty (Dirs *inicio, int dirId);
+void ShowTimes();
+int VerifyChilds (Dirs *inicio, int dirId);
+char* ReturnNameProperty (Users *inicio, int dirId);
 
 /* Variaveis global */
 int id=0, currentDir = 0, current_user = 0, userId=1;
@@ -129,6 +133,9 @@ int main(void)
     if(strcmp (comando, "date") == 0) {
         ShowDateTime();
     }
+    if(strcmp (comando, "time") == 0) {
+        ShowTimes();
+    }
     if(strcmp (comando, "copyright") == 0) {
         Copyright();
     }
@@ -142,7 +149,7 @@ int main(void)
     //Remove(&ini, "test");
     //printf("%s", ini.name);
     if(strcmp (comando, "ls") == 0) {
-        Listar(&ini);
+        Listar(&ini, &ini_users);
     }
     if(strcmp (comando, "pwd") == 0) {
         PWD(&ini);
@@ -199,6 +206,49 @@ int main(void)
     
 }
 
+int VerifyProperty (Dirs *inicio, int dirId) {
+    int id = 0;
+    Dirs *percorre;
+    percorre = inicio;
+    //printf("%d \n", current_user);
+        //entrar
+        while (1 > 0)
+        {
+            //printf("\n Nome: %s | pproperty: %d | ID: %d \n", percorre->name, percorre->property_id, percorre->id);
+            //printf("\n ID: %d \n", percorre->id);
+            if(percorre->id == dirId) {
+                id = percorre->property_id;
+                break;
+            }
+            if(percorre->prox == NULL) {break;}
+            percorre = percorre -> prox;
+        }
+    
+    return id;
+}
+
+
+int VerifyChilds (Dirs *inicio, int dirId) {
+    int count = 0;
+    Dirs *percorre;
+    percorre = inicio;
+    //printf("%d \n", current_user);
+    //entrar
+    while (1 > 0)
+    {
+        //printf("\n Nome: %s | pproperty: %d | ID: %d \n", percorre->name, percorre->property_id, percorre->id);
+        //printf("\n ID: %d \n", percorre->id);
+        if(percorre->father_id == dirId) {
+            count++;
+            break;
+        }
+        if(percorre->prox == NULL) {break;}
+        percorre = percorre -> prox;
+    }
+    
+    return count;
+}
+
 
 void Inicializar_Dirs (Dirs *inicio)
 {
@@ -211,7 +261,7 @@ void Inicializar_Dirs (Dirs *inicio)
 
 void Inicializar_Users (Users *users)
 {
-    users->id = 1;
+    users->id = userId;
     strcpy(users->name, "ROOT");
     strcpy(users->login, "root");
     users->status = 1;
@@ -228,7 +278,12 @@ void Inserir (Dirs *inicio)
     Dirs *no_novo = (Dirs *) malloc(sizeof(Dirs));
     no_novo -> id = id;
     no_novo -> father_id = currentDir;
-    no_novo -> property_id = userId;
+    no_novo -> property_id = current_user;
+    char datetime[50];
+    strcpy(datetime, __DATE__);
+    strcat(datetime, " ");
+    strcat(datetime, __TIME__);
+    strcpy(no_novo->datetime, datetime);
     strcpy(no_novo->name, parans);
     no_novo -> prox = NULL;
     
@@ -251,6 +306,11 @@ void InsertHome (Dirs *inicio, char* name)
     no_novo -> id = id;
     no_novo -> father_id = 0;
     no_novo -> property_id = userId;
+    char datetime[50];
+    strcpy(datetime, __DATE__);
+    strcat(datetime, " ");
+    strcat(datetime, __TIME__);
+    strcpy(no_novo->datetime, datetime);
     strcpy(no_novo->name, name);
     no_novo -> prox = NULL;
     
@@ -264,14 +324,14 @@ void InsertHome (Dirs *inicio, char* name)
 }
 
 
-void Listar (Dirs *inicio)
+void Listar (Dirs *inicio, Users *ini_users)
 {
     Dirs *percorre;
     percorre = inicio;
     while (1 > 0)
     {
         if(percorre->father_id == currentDir && strcmp (percorre->name, "/") != 0) {
-            printf("       %s \n", percorre->name);
+            printf("       %s   | %s   | %s  \n", percorre->name, percorre->datetime, ReturnNameProperty (ini_users, percorre->property_id));
         }
         //printf("\n %s", percorre->name);
         
@@ -290,20 +350,22 @@ void Remove (Dirs *inicio)
     while (1 > 0)
     {
         if(percorre->father_id == currentDir && strcmp (percorre->name, parans) == 0 &&percorre->id !=0) {
-            
-        //if(percorre->id == 1) {
-            if(percorre->prox == NULL) {
-                percorreAnt -> prox = NULL;
-                free(percorre);
+            i++;
+            if(VerifyChilds (inicio, percorre->id) == 0) {
+                if(percorre->prox == NULL) {
+                    percorreAnt -> prox = NULL;
+                    free(percorre);
+                } else {
+                    //meio
+                    percorreAnt->prox = percorre->prox;
+                }
             } else {
-                //meio
-                percorreAnt->prox = percorre->prox;
+                printf("Impossivel remover pois esse diretorio tem subdiretorios \n");
             }
         }
         if(percorre->prox == NULL) {break;}
         percorreAnt = percorre;
         percorre = percorre -> prox;
-        i++;
     }
   
 }
@@ -348,17 +410,25 @@ void CD (Dirs *inicio)
         //ir ao pai
         while (1 > 0)
         {
-            if(percorre->id == currentDir) {
-                currentDir = percorre->father_id;
+            if(percorre->id == currentDir ) {
+//                printf("Proprietario: %d \n", VerifyProperty(inicio, percorre->father_id));
+                if(VerifyProperty(inicio, percorre->father_id) == current_user || ( VerifyProperty(inicio, percorre->father_id) == 1) && current_user == 1 ) {
+                        currentDir = percorre->father_id;
+                } else {
+                    //printf("Voce esta tentando acessar um diretorio que nao e seu %d %d \n", userId, VerifyProperty(&inicio, percorre->father_id));
+                    printf("Voce esta tentando acessar um diretorio que nao e seu\n");
+                }
+                
             }
             if(percorre->prox == NULL) {break;}
             percorre = percorre -> prox;
         }
     } else {
         //entrar
+        //n precisa de permissao pois o user q entrou n consegue subir sem ser o root
         while (1 > 0)
         {
-            if(percorre->father_id == currentDir && strcmp (percorre->name, parans) == 0 &&percorre->id !=0) {
+            if(percorre->father_id == currentDir && strcmp (percorre->name, parans) == 0 &&percorre->id !=0 ) {
                 currentDir = percorre->id;
             }
             if(percorre->prox == NULL) {break;}
@@ -415,7 +485,13 @@ void userInfo (Users *inicio) {
     {
         if(strcmp (percorre->login, parans) == 0) {
             i++;
-            printf("\n Nome: %s \n", percorre->name);
+            char status[50];
+            if(percorre->status == 0) {
+                strcpy(status, "Bloqueado");
+            } else {
+                strcpy(status, "Ativo");
+            }
+            printf("\n Nome: %s   |   Status: %s \n", percorre->name, status);
         }
         if(percorre->prox == NULL) {break;}
         percorre = percorre -> prox;
@@ -423,6 +499,24 @@ void userInfo (Users *inicio) {
     if(i == 0) {
         printf("Usuario nao localizado \n");
     }
+}
+
+char* ReturnNameProperty (Users *inicio, int dirId) {
+    Users *percorre;
+    char* name;
+    percorre = inicio;
+    int i =0; //count of localized user
+    while (1 > 0)
+    {
+        if(percorre->id == dirId) {
+            i++;
+            strcpy(name, percorre->name);
+        }
+        if(percorre->prox == NULL) {break;}
+        percorre = percorre -> prox;
+    }
+    
+    return name;
 }
 
 void BlockUsername (Users *inicio) {
@@ -473,7 +567,12 @@ void UnlockUsername (Users *inicio) {
 }
 
 void ShowDateTime () {
-    printf(" \n DATA : %s HORA: %s \n",__DATE__,__TIME__);
+    printf(" \n DATA : %s \n",__DATE__);
+}
+
+void ShowTimes() {
+    printf("HORA: %s \n", __TIME__);
+    
 }
 
 void Copyright () {
